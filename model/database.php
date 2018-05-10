3d7b8f3ebccfd837cca6003086202fb98a9bb1ef
Jhonny182012 <38548607+Jhonny182012@users.noreply.github.com>
2018-05-10 08:56:28 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=localhost:12;dbname=bdd;charset=utf8', 'user', 'pass');
+        $pdo = new PDO('mysql:host=alumnos-jhonatan.mysql.database.azure.com;dbname=addalumno;charset=utf8', 'jmdominguez@alumnos-jhonatan', 'orejitasxD182012');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
